0f0e855e74715f79d7b708826eb7aab74343590c
Dan Pallas <git@danpallas.com>
2015-10-23 17:43:42 -0400

    stop sending auth token with config call and handle accessToken authorizatioToken mismatch.

---
+++ library/src/main/java/io/oddworks/device/exceptions/OddAuthTokenUserMismatch.java
@@ -0,0 +1,23 @@
+package io.oddworks.device.exceptions;
+
+/**
+ * Created by dan on 10/23/15.
+ */
+public class OddAuthTokenUserMismatch extends BadResponseCodeException {
+
+    public OddAuthTokenUserMismatch(int code) {
+        super(code);
+    }
+
+    public OddAuthTokenUserMismatch(String detailMessage, int code) {
+        super(detailMessage, code);
+    }
+
+    public OddAuthTokenUserMismatch(String detailMessage, Throwable throwable, int code) {
+        super(detailMessage, throwable, code);
+    }
+
+    public OddAuthTokenUserMismatch(Throwable throwable, int code) {
+        super(throwable, code);
+    }
+}
