7751a6b0a64297d8ff18d42edfcb75f332ff5bfb
Erik Straub <erik@madglory.com>
2015-12-10 16:24:16 -0500

    Adds OddMetricHandler handler for OddMetric post over OddBus

---
+++ OddWorksDeviceSDK/src/main/java/io/oddworks/device/handler/OddMetricHandler.java
@@ -0,0 +1,39 @@
+package io.oddworks.device.handler;
+
+import android.util.Log;
+
+import com.squareup.otto.Subscribe;
+
+import io.oddworks.device.metric.OddMetric;
+import io.oddworks.device.request.ApiCaller;
+import io.oddworks.device.request.OddCallback;
+import io.oddworks.device.request.RestServiceProvider;
+import io.oddworks.device.service.OddBus;
+
+public class OddMetricHandler {
+    private static final String TAG = OddMetricHandler.class.getSimpleName();
+    private static final OddMetricHandler INSTANCE = new OddMetricHandler();
+
+    private OddMetricHandler() {
+        // singleton
+        OddBus.getInstance().register(this);
+    }
+
+    @Subscribe
+    public void handleOddMetric(OddMetric metric) {
+        RestServiceProvider restServiceProvider = RestServiceProvider.getInstance();
+        ApiCaller apiCaller = restServiceProvider.getApiCaller();
+
+        apiCaller.postMetric(metric, new OddCallback<OddMetric>() {
+            @Override
+            public void onSuccess(OddMetric entity) {
+                Log.d(TAG, "handleOddMetric: SUCCESS " + entity.toString());
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                Log.d(TAG, "handleOddMetric: FAILURE " + exception.toString());
+            }
+        });
+    }
+}
