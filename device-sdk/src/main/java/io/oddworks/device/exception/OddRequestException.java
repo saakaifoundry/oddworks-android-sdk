50ecec47e9767611bb63b38a6c15ddab53e59e79
Erik Straub <erik.straub@oddnetworks.com>
2016-10-17 15:36:31 -0400

    Removes unnecessary constructors

--- device-sdk/src/main/java/io/oddworks/device/exception/OddRequestException.java
+++ device-sdk/src/main/java/io/oddworks/device/exception/OddRequestException.java
@@ -7,12 +7,4 @@ public class OddRequestException extends RuntimeException {
     public OddRequestException(String detailMessage) {
         super(detailMessage);
     }
-
-    public OddRequestException(String detailMessage, Throwable throwable) {
-        super(detailMessage, throwable);
-    }
-
-    public OddRequestException(Throwable throwable) {
-        super(throwable);
-    }
 }
